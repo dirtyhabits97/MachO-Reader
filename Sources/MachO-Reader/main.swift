a033b2aa0dac74c93a48d73c88657449b728bc9b
dirtyhabits97 <gonzalorehu@gmail.com>
2022-06-09 00:19:08 -0500

    Added fatHeader and header flags to Reader command.

--- Sources/MachO-Reader/main.swift
+++ Sources/MachO-Reader/main.swift
@@ -3,15 +3,14 @@ import ArgumentParser
 
 struct Reader: ParsableCommand {
 
-    // TODO: figure out what to do with these
-    // @Flag(name: [.customShort("f"), .customLong("fat")], help: "Only print the fat header.")
-    // var onlyFatHeader: Bool = false
-
-    // @Flag(name: [.customShort("h"), .customLong("header")], help: "Only print the mach-o header.")
-    // var onlyHeader: Bool = false
-
     @Option(help: "The arch of the mach-o header to read.")
     var arch: String?
+
+    @Flag(name: .shortAndLong, help: "Only outputs information for the fat header.")
+    var fatHeader: Bool = false
+
+    @Flag(name: .shortAndLong, help: "Only outputs information for the mach-o header.")
+    var header: Bool = false
 
     @Flag(help: "Only outputs information for the segment commands.")
     var segments: Bool = false
@@ -25,7 +24,17 @@ struct Reader: ParsableCommand {
             return
         }
         let file = try MachOFile(from: url, arch: arch)
-        // if only print segments
+        // only print fat header
+        if fatHeader, let header = file.fatHeader {
+            CLIFormatter.print(header)
+            return
+        }
+        // only print mach-o header
+        if header {
+            CLIFormatter.print(file.header)
+            return
+        }
+        // only print segments
         if segments {
             file.commands
                 .compactMap({ (loadCommand: LoadCommand) -> SegmentCommand? in
