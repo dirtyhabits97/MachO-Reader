29b98feb5ff68a81b9c01b92b84d534be2b665d2
dirtyhabits97 <gonzalorehu@gmail.com>
2022-06-11 18:14:25 -0500

    Added some documentation.

--- Sources/MachOReaderLib/Models/LoadCommandTypeRepresentable.swift
+++ Sources/MachOReaderLib/Models/LoadCommandTypeRepresentable.swift
@@ -1,15 +1,25 @@
 import Foundation
 
-// TODO: document this
+/// A Type that can be built from MachO's LoadCommand object.
 protocol LoadCommandTypeRepresentable {
 
+    /// The cmds that can correctly build this Type.
+    ///
+    /// Example: `LC_SEGMENT` can build a SegmentCommand but not a
+    /// BuildVersionCommand.
     static var allowedCmds: Set<Cmd> { get }
 
+    /// Builds a `LoadCommandType` wrapper that contains this type.
+    ///
+    /// - parameter loadCommand: the loadCommand that contains the data
+    /// to build this object.
     static func build(from loadCommand: LoadCommand) -> LoadCommandType
 }
 
 extension LoadCommand {
 
+    /// Returns `true` if the `LoadCommand` can be used to create
+    /// the `type` parameter.
     func `is`(_ type: LoadCommandTypeRepresentable.Type) -> Bool {
         type.allowedCmds.contains(cmd)
     }
