c5402e77556d7ca0a32e3831b760d6bcca87500d
dirtyhabits97 <gonzalorehu@gmail.com>
2022-07-08 23:16:46 -0500

    Added DYLD_CHAINED_PTR_ARM64E models.

--- Sources/MachOReaderLib/CModels.swift
+++ Sources/MachOReaderLib/CModels.swift
@@ -114,6 +114,56 @@ typealias dyld_chained_ptr_64_bind = UInt64
 // };
 typealias dyld_chained_ptr_64_rebase = UInt64
 
+// DYLD_CHAINED_PTR_ARM64E
+// struct dyld_chained_ptr_arm64e_rebase
+// {
+//     uint64_t    target   : 43,
+//                 high8    :  8,
+//                 next     : 11,    // 4 or 8-byte stide
+//                 bind     :  1,    // == 0
+//                 auth     :  1;    // == 0
+// };
+typealias dyld_chained_ptr_arm64e_rebase = UInt64
+
+// DYLD_CHAINED_PTR_ARM64E
+// struct dyld_chained_ptr_arm64e_bind
+// {
+//     uint64_t    ordinal   : 16,
+//                 zero      : 16,
+//                 addend    : 19,    // +/-256K
+//                 next      : 11,    // 4 or 8-byte stide
+//                 bind      :  1,    // == 1
+//                 auth      :  1;    // == 0
+// };
+typealias dyld_chained_ptr_arm64e_bind = UInt64
+
+// DYLD_CHAINED_PTR_ARM64E
+// struct dyld_chained_ptr_arm64e_auth_rebase
+// {
+//     uint64_t    target    : 32,   // runtimeOffset
+//                 diversity : 16,
+//                 addrDiv   :  1,
+//                 key       :  2,
+//                 next      : 11,    // 4 or 8-byte stide
+//                 bind      :  1,    // == 0
+//                 auth      :  1;    // == 1
+// };
+typealias dyld_chained_ptr_arm64e_auth_rebase = UInt64
+
+// DYLD_CHAINED_PTR_ARM64E
+// struct dyld_chained_ptr_arm64e_auth_bind
+// {
+//     uint64_t    ordinal   : 16,
+//                 zero      : 16,
+//                 diversity : 16,
+//                 addrDiv   :  1,
+//                 key       :  2,
+//                 next      : 11,    // 4 or 8-byte stide
+//                 bind      :  1,    // == 1
+//                 auth      :  1;    // == 1
+// };
+typealias dyld_chained_ptr_arm64e_auth_bind = UInt64
+
 // DYLD_CHAINED_PTR_32
 // struct dyld_chained_ptr_32_bind
 // {
