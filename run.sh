75ea6e84461b30112c76fcc92be74ba99fc25d91
Gonzalo <gonzalorehu@gmail.com>
2024-04-19 12:58:31 -0500

    Improved run script and added test script.

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
-swift build -c "release" --disable-sandbox
-./.build/release/macho-reader "$@"
+# If bazel is installed
+if command -v bazel &> /dev/null; then
+  bazel run //Sources/MachOReaderCLI:MachOReaderCLI "$@"
+# Default to spm
+else
+  swift build -c "release" --disable-sandbox
+  ./.build/release/macho-reader "$@"
+fi
