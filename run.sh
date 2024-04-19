47f4e724b269ea25ac5d94484a057ffb59b23e00
Gonzalo <gonzalorehu@gmail.com>
2024-04-19 13:15:42 -0500

    fixup! Fix testing with Bazel (#15)

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # If bazel is installed
 if command -v bazel &> /dev/null; then
-  bazel run //Sources/MachOReaderCLI:MachOReaderCLI "$@"
+  bazel run //Sources/MachOReaderCLI:MachOReaderCLI -- "$@"
 # Default to spm
 else
   swift build -c "release" --disable-sandbox
