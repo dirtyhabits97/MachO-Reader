31231308419f26ce5f25f0986eb380defce19d3a
Gonzalo <gonzalorehu@gmail.com>
2024-04-20 00:48:43 -0500

    Uncommented DyldChainedFixupsCommand.

--- Sources/MachOReaderCLI/Commands/DyldChainedFixups.swift
+++ Sources/MachOReaderCLI/Commands/DyldChainedFixups.swift
@@ -1,59 +1,59 @@
-// import ArgumentParser
-// import Foundation
-// import MachOReaderLib
-// TODO: restore this
+import ArgumentParser
+import Foundation
+import MachOReaderLib
 
-// struct DyldChainedFixupsCommand: ParsableCommand {
+struct DyldChainedFixupsCommand: ParsableCommand {
 
-//     static let configuration = CommandConfiguration(commandName: "chained-fixups")
+    static let configuration = CommandConfiguration(commandName: "chained-fixups")
 
-//     // MARK: - Properties
+    // MARK: - Properties
 
-//     @Option(help: "The arch of the mach-o header to read.")
-//     var arch: String?
+    @Option(help: "The arch of the mach-o header to read.")
+    var arch: String?
 
-//     @Flag(help: "Only outputs the chained header.")
-//     var chainedHeader: Bool = false
+    @Flag(help: "Only outputs the chained header.")
+    var chainedHeader: Bool = false
 
-//     @Flag(help: "Only outputs the imports.")
-//     var imports: Bool = false
+    @Flag(help: "Only outputs the imports.")
+    var imports: Bool = false
 
-//     @Flag(help: "Only outputs the pages.")
-//     var pages: Bool = false
+    @Flag(help: "Only outputs the pages.")
+    var pages: Bool = false
 
-//     @Argument(help: "The binary to inspect.")
-//     var pathToBinary: String
+    @Argument(help: "The binary to inspect.")
+    var pathToBinary: String
 
-//     // MARK: - Methods
+    // MARK: - Methods
 
-//     func run() throws {
-//         guard let url = URL(string: "file://\(pathToBinary)") else {
-//             print("Could not create url for \(pathToBinary)")
-//             return
-//         }
-//         let file = try MachOFile(from: url, arch: arch)
-//         let report = file.dyldChainedFixupsReport()
+    func run() throws {
+        guard
+            let url = URL(string: "file://\(pathToBinary)")
+        else {
+            fatalError(("Could not create url for \(pathToBinary)"))
+        }
 
-//         if chainedHeader {
-//             CLIFormatter.print(report.header)
-//             return
-//         }
+        let file = try MachOFile(from: url, arch: arch)
+        let report = file.dyldChainedFixupsReport()
 
-//         if imports {
-//             for imp in report.imports {
-//                 CLIFormatter.print(imp)
-//             }
-//             return
-//         }
+        if chainedHeader {
+            return CLIFormatter.print(report.header)
+        }
 
-//         if pages {
-//             for (segmentInfo, pages) in zip(report.segmentInfo, report.pageInfo()) {
-//                 CLIFormatter.print(segmentInfo)
-//                 CLIFormatter.print(pages)
-//             }
-//             return
-//         }
+        if imports {
+            for imp in report.imports {
+                CLIFormatter.print(imp)
+            }
+            return
+        }
 
-//         CLIFormatter.print(file.dyldChainedFixupsReport())
-//     }
-// }
+        if pages {
+            for (segmentInfo, pages) in zip(report.segmentInfo, report.pageInfo()) {
+                CLIFormatter.print(segmentInfo)
+                CLIFormatter.print(pages)
+            }
+            return
+        }
+
+        CLIFormatter.print(file.dyldChainedFixupsReport())
+    }
+}
